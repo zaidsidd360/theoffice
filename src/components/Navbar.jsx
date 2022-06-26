3a559b4217cb62df260924eef4f32cf6f705fdf2
zaidsidd360 <87397035+zaidsidd360@users.noreply.github.com>
2022-06-26 16:37:43 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/Navbar.css";
 import { NavLink } from "react-router-dom";
-// import styled from "styled-components";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import Logo from "../assets/officelogo.png";
@@ -12,12 +11,6 @@ function Navbar() {
   const closeNavOnClick = () => {
     setToggleNav(false);
   };
-
-  // document.body.addEventListener("click", () => {
-  //   if (toggleNav === true) {
-  //     setToggleNav(false);
-  //   }
-  // });
 
   return (
     <>
@@ -66,66 +59,5 @@ function Navbar() {
     </>
   );
 }
-
-// const Nav = styled.nav`
-//   /* display: flex; */
-//   /* justify-content: space-between; */
-//   /* align-items: center; */
-//   height: 95px;
-//   /* width: 100vw; */
-//   position: sticky;
-//   top: 0;
-//   z-index: 10;
-//   background-color: #000000;
-//   /* box-shadow: 0 5px 10px rgb(0, 0, 0); */
-// `;
-
-// const List = styled.li`
-//   padding: 10px;
-//   padding-left: 0;
-//   padding-bottom: 0;
-//   margin-inline: 5px;
-//   color: #000000;
-//   text-decoration: none;
-//   transition: all 200ms ease;
-// `;
-
-// const Item = styled(NavLink)`
-//   color: #ffffff;
-//   text-decoration: none;
-//   transition: all 400ms ease;
-//   &.active {
-//     color: #5e5e5e;
-//     &:hover {
-//       text-decoration: none;
-//     }
-//   }
-//   &:hover {
-//     text-decoration: underline;
-//     cursor: pointer;
-//   }
-// `;
-
-// const UList = styled.ul`
-//   align-self: center;
-//   list-style: none;
-//   display: flex;
-//   align-items: center;
-//   justify-content: right;
-//   font-size: 30px;
-//   margin-inline: 100px;
-//   margin-top: 0;
-//   margin-bottom: 0;
-//   padding-right: 40px;
-//   @media (max-width: 875px) {
-//     margin-inline: 0;
-//     padding-right: 0;
-//     flex-direction: column;
-//     /* padding-top: 45px; */
-//     align-self: baseline;
-//     align-items: flex-start;
-//     /* padding-left: 0; */
-//   }
-// `;
 
 export default Navbar;
